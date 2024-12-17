3c6de6e03e99ce3c67d12400380d3b8f0569c587
Dilshodbek <Dilshoddeveloper2005@gmail.com>
2024-12-17 21:04:08 +0500

    Python codes

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
-from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+
+def generate_spectrogram(wav_file):
+    sample_rate, data = wavfile.read(wav_file)
+
+    if len(data.shape) > 1:
+        data = data[:, 0]
+    plt.figure(figsize=(12, 6))
+    plt.specgram(data, Fs=sample_rate, NFFT=1024, noverlap=512, cmap='inferno')
+    plt.title(f'Spectrogram of {wav_file}')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    plt.colorbar(label='Intensity (dB)')
+    plt.xlim(0, len(data) / sample_rate)
+    plt.ylim(0, sample_rate / 2)  
+
+    plt.grid(True)
+
+    plt.show()
+wav_file_path = 'my_file.wav'
+generate_spectrogram(wav_file_path)
+
+
+#kiritigan mattni o'qish
+from gtts import gTTS
 import os
 
-rasmlar = ["rasm1.jpg", "rasm2.jpg", "rasm3.jpg"]
+text = "You are very good student"
 
-for rasm_nomi in rasmlar:
-    try:
-       
-        rasm = Image.open(rasm_nomi)
-        
-        oq_qora_rasm = rasm.convert("L")
-        
-        yangi_nomi = f"oq_qora_{rasm_nomi}"
-        
-        oq_qora_rasm.save(yangi_nomi)
-        
-        oq_qora_rasm.show()
-        
-        print(f"{rasm_nomi} -> {yangi_nomi} ga aylantirildi va ko'rsatildi.")
-    except Exception as e:
-        print(f"{rasm_nomi} fayli bilan muammo: {e}")
+language = 'en'
+tts = gTTS(text=text, lang=language, slow=False)
+
+audio_file = "output.mp3"
+tts.save(audio_file)
+
+from IPython.display import Audio
+Audio(audio_file)
