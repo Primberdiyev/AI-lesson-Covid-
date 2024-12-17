43188fbf6359cd0a6db6775198a14f3f5de2a1d0
Dilshodbek <Dilshoddeveloper2005@gmail.com>
2024-12-17 21:33:15 +0500

    dataset codes

--- main.py
+++ main.py
@@ -1,39 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
+import pandas as pd
 
-def generate_spectrogram(wav_file):
-    sample_rate, data = wavfile.read(wav_file)
+df = pd.read_csv('sales.csv')
 
-    if len(data.shape) > 1:
-        data = data[:, 0]
-    plt.figure(figsize=(12, 6))
-    plt.specgram(data, Fs=sample_rate, NFFT=1024, noverlap=512, cmap='inferno')
-    plt.title(f'Spectrogram of {wav_file}')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Frequency (Hz)')
-    plt.colorbar(label='Intensity (dB)')
-    plt.xlim(0, len(data) / sample_rate)
-    plt.ylim(0, sample_rate / 2)  
+print("Datasetning birinchi 5 qatori:")
+print(df.head()) 
 
-    plt.grid(True)
+print("\nStatistikalar:")
+print(df.describe())  
+avg_unit_price = df['unit_price'].mean()
+print(f"O‘rtacha narx: {avg_unit_price}")
 
-    plt.show()
-wav_file_path = 'my_file.wav'
-generate_spectrogram(wav_file_path)
+df['total_sales'] = df['unit_price'] * df['quantity'] 
+total_sales = df['total_sales'].sum()
+print(f"Jami savdo hajmi: {total_sales}")
 
 
-#kiritigan mattni o'qish
-from gtts import gTTS
-import os
-
-text = "You are very good student"
-
-language = 'en'
-tts = gTTS(text=text, lang=language, slow=False)
-
-audio_file = "output.mp3"
-tts.save(audio_file)
-
-from IPython.display import Audio
-Audio(audio_file)
+top_selling_products = df.groupby('product_name')['quantity'].sum().sort_values(ascending=False).head(5)
+print("\nEng ko‘p sotilgan 5 mahsulot:")
+print(top_selling_products)
